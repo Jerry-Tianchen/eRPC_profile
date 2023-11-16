901a97a212198f90e10d8979972cbafe3586638b
Tianchen Wang <wangtianchenjerry@gmail.com>
2023-11-16 14:50:06 -0600

    v2

--- src/rpc.h
+++ src/rpc.h
@@ -830,9 +830,9 @@ class Rpc {
   /// Copy the data from a packet to a MsgBuffer at a packet index
   static inline void copy_data_to_msgbuf(MsgBuffer *msgbuf, size_t pkt_idx,
                                          const pkthdr_t *pkthdr) {
-    size_t offset = pkt_idx * TTr::kMaxDataPerPkt;
-    size_t to_copy =
-        (std::min)(TTr::kMaxDataPerPkt, pkthdr->msg_size_ - offset);
+    // size_t offset = pkt_idx * TTr::kMaxDataPerPkt;
+    // size_t to_copy =
+        // (std::min)(TTr::kMaxDataPerPkt, pkthdr->msg_size_ - offset);
     // memcpy(&msgbuf->buf_[offset], pkthdr + 1, to_copy);  // From end of pkthdr
   }
 
