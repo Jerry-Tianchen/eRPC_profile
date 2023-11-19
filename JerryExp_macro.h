afcd9027f50a89d7ec32487c4fbb71e339092f38
Tianchen Wang <wangtianchenjerry@gmail.com>
2023-11-18 21:36:32 -0600

    v1

--- JerryExp_macro.h
+++ JerryExp_macro.h
@@ -4,6 +4,7 @@
 
 #define DISABLE_process_large_req_one_st_copy
 #define DISABLE_process_resp_one_st_copy
-
+#define START_TIMER uint64_t timmer_profile_start = erpc::rdtsc();
+#define SHOW_TIMER printf("Time is %f\n", erpc::to_usec(erpc::rdtsc() - timmer_profile_start, 2));
 
 #endif
